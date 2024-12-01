22a550eae8d7fcddf509780f09e27a1609f96f39
Liu Siyao <liusiyao.sine@bytedance.com>
2024-12-02 00:58:09 +0800

    feat: install scripts without mirror

--- runtime/python/install-python-runtime.sh
+++ runtime/python/install-python-runtime.sh
@@ -1,17 +1,29 @@
+#!/bin/bash
 set -o errexit
+
+USE_OFFICIAL_SOURCE=0
+for arg in "$@"
+do
+    if [ "$arg" = "us" ]; then
+        USE_OFFICIAL_SOURCE=1
+    fi
+done
 
 rm -f ~/.condarc
 conda create -n sandbox-runtime -y python=3.10
 
 source activate sandbox-runtime
 
-pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+if [ $USE_OFFICIAL_SOURCE -eq 0 ]; then
+    pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+fi
+
 pip install -r ./requirements.txt --ignore-requires-python
 
 # for NaturalCodeBench python problem 29
 python -c "import nltk; nltk.download('punkt')"
 
-# for CIBench nltk problems
+# for CIBench nltk problems 
 python -c "import nltk; nltk.download('stopwords')"
 
 pip cache purge
--- scripts/install-miniconda.sh
+++ scripts/install-miniconda.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 py_ver="$1"
+location="$2"
 FILENAME=Miniconda3-py${py_ver//.}_23.5.2-0-Linux-x86_64.sh
 
 if [ "$1" = "3.7" ]
@@ -9,7 +10,14 @@ then
 FILENAME=Miniconda3-py${py_ver//.}_23.1.0-1-Linux-x86_64.sh
 fi
 
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/${FILENAME}
-mkdir /root/.conda
+if [ "$location" = "us" ]
+then
+  MIRROR="https://repo.anaconda.com/miniconda"
+else
+  MIRROR="https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda"
+fi
+
+wget ${MIRROR}/${FILENAME}
+mkdir -p /root/.conda
 bash ${FILENAME} -b
 rm -f ${FILENAME}
