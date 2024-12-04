0e266ba3b9b390776a8d8f0a721aa0e1fff63428
Liu Siyao <liusiyao.sine@bytedance.com>
2024-12-04 23:50:16 +0800

    docs: better examples

--- docs/src/components/DatasetSelector/docGenerator.tsx
+++ docs/src/components/DatasetSelector/docGenerator.tsx
@@ -1,19 +1,19 @@
 import { constants } from '@site/src/constants';
 import CodeBlock from '@theme/CodeBlock';
 
-export const docWithType = ({selectedDataset, datasetType, config, generatePythonDict}) => {
-    config.dataset_type = datasetType;
-    const pythonConfig = generatePythonDict(config);
-    let load_stmt = `load_dataset("${selectedDataset.huggingFace.id}", split="${selectedDataset.huggingFace.split}")`
-    if (selectedDataset.huggingFace.subset) {
-      load_stmt = `load_dataset("${selectedDataset.huggingFace.id}", "${selectedDataset.huggingFace.subset}", split="${selectedDataset.huggingFace.split}")`
-    }
-    return  <>
-      <p>
-        Get prompts:
-      </p>
-      <CodeBlock language="python">
-        {`from datasets import load_dataset
+export const docWithType = ({ selectedDataset, datasetType, config, generatePythonDict }) => {
+  config.dataset_type = datasetType;
+  const pythonConfig = generatePythonDict(config);
+  let load_stmt = `load_dataset("${selectedDataset.huggingFace.id}", split="${selectedDataset.huggingFace.split}")`
+  if (selectedDataset.huggingFace.subset) {
+    load_stmt = `load_dataset("${selectedDataset.huggingFace.id}", "${selectedDataset.huggingFace.subset}", split="${selectedDataset.huggingFace.split}")`
+  }
+  return <>
+    <p>
+      Get prompts:
+    </p>
+    <CodeBlock language="python">
+      {`from datasets import load_dataset
 import requests
 
 config = ${pythonConfig}
@@ -25,25 +25,29 @@ config['provided_data'] = data
 prompts = requests.post('${constants.host}/get_prompts', json={
   'dataset': '${selectedDataset.dataset}',
   'config': config
-})`}
-      </CodeBlock>
-      <p>
-        Get results after model inference:
-      </p>
-      <CodeBlock language="python">
-        {`config['provided_data'] = data[0]
-res = requests.post('${constants.host}/submit', json={
-  'dataset': '${selectedDataset.dataset}',
-  'id': data[0]['id'],
-  'completion': completion,
-  'config': config
-})
+}).json()
 
-print(f'result: {res.json()}')`}
-      </CodeBlock>
-      <p>
-        Note: always put raw completion in the request, Sandbox will handle the extraction of code according to different modes.
-      </p>
-    </>
-  };
-  
+print('please perform model inference on these prompts:')
+print('\\n'.join([p['prompt'] for p in prompts[:3]]))
+print('...')
+
+# your model inference code here
+completions = ['' for _ in prompts]
+
+for completion, sample in zip(completions, data):
+    config['provided_data'] = sample
+    res = requests.post('${constants.host}/submit', json={
+        'dataset': '${selectedDataset.dataset}',
+        'id': '',
+        'completion': completion,
+        'config': config
+    })
+
+    print(f'result: {res.json()}')
+    break`}
+    </CodeBlock>
+    <p>
+      Note: always put raw completion in the request, Sandbox will handle the extraction of code according to different modes.
+    </p>
+  </>
+};
